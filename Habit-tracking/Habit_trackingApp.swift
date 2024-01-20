fde35ae6aab460d4ed82f8cd8db54ddfe9bc80da
leotn03 <leo.tn03@icloud.com>
2024-01-20 16:42:12 -0500

    Initial Commit

---
+++ Habit-tracking/Habit_trackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_trackingApp.swift
+//  Habit-tracking
+//
+//  Created by Leo Torres Neyra on 20/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Habit_trackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
